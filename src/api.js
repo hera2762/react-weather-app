21b5f05de0a182af335165d5656005ada16e44ce
Hera Aijaz <aijazhera.2762@gmail.com>
2022-07-16 01:41:53 +0530

    Base Application

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3cd5f679c8mshc61f15d216e2466p16c5fejsn2cf5ecb565a8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "f54ecf89defc6209743423266744c67c";
