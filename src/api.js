e131de8f9621b222a44263783d163d7761df23a0
Hera Aijaz <65110343+hera2762@users.noreply.github.com>
2022-07-18 23:07:35 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '//your own API Key',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,4 +9,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "f54ecf89defc6209743423266744c67c";
+export const WEATHER_API_KEY = "";
